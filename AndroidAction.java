4aee66c3c4242d7c7a9cdb599af87f48b152d94d
coobbi <138000094+coobbi@users.noreply.github.com>
2024-08-17 22:19:32 +0800

    Create AndroidAction.java

---
+++ AndroidAction.java
@@ -0,0 +1,16 @@
+package com.coobbi.android.api;
+
+public class AndroidAction {
+    //This uses the native camera to take pictures.
+    public static final String IMAGE_CAPTURE_ANDROID = "ACTION_IMAGE_CAPTURE_ANDROID";
+    //A simple photo taking function
+    public static final String IMAGE_CAPTURE = "ACTION_IMAGE_CAPTURE";
+    //Video recording function
+    public static final String VIDEO_CAPTURE = "ACTION_VIDEO_CAPTURE";
+    //Android Notifications
+    public static final String CREATE_NOTIFICATION = "ACTION_CREATE_NOTIFICATION";
+    //Get geographic location
+    public static final String GET_LOCATION = "ACTION_GET_LOCATION";
+    //play video
+    public static final String PLAY_VIDEO = "ACTION_PLAY_VIDEO";
+}
